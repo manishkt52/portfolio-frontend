0cb1cf0d195a761c813b165c0f5636df6e98ef18
manishkt52 <manishkt52@gmail.com>
2026-02-28 07:34:38 +0530

    Remove Sentry Files 28-02-2026 07:34

--- app/global-error.jsx
+++ app/global-error.jsx
@@ -1,18 +1,23 @@
 "use client";
 
-import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
-import { useEffect } from "react";
 
-export default function GlobalError({ error }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
-
+export default function GlobalError({ error, reset }) {
   return (
     <html>
       <body>
-        <Error />
+        <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+          <h2 className="text-2xl font-bold mb-4">
+            Something went wrong!
+          </h2>
+
+          <button
+            onClick={() => reset()}
+            className="px-4 py-2 bg-purple-600 rounded-lg hover:bg-purple-700 transition"
+          >
+            Try Again
+          </button>
+        </div>
       </body>
     </html>
   );
